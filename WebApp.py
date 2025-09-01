bd958b5bc8c667f33193a9de64095a8b0bf5c4f1
itamarw <itamar14@gmail.com>
2025-09-01 13:07:49 +0300

    Built Webapp backend integration

--- WebApp.py
+++ WebApp.py
@@ -1,4 +1,6 @@
 import os
+import json
+import csv
 from flask import (
     Flask,
     render_template,
@@ -7,6 +9,7 @@ from flask import (
     url_for,
     send_from_directory,
 )
+from flask_sse import sse
 from Core import parse_files, push_config, verify
 
 
@@ -23,9 +26,38 @@ def upload():
     return render_template("upload.html")
 
 
-@app.route("/validate_device", methods=["POST"])
-def validate_device():
-    data = request.get_json()
+@app.route("/start_rollout", methods=["POST", "GET"])
+def start_rollout():
+    #File uploads
+    device_file = request.files.get("device_file")
+    commands_file = request.files.get("commands_file")
+
+    #Manual Entry
+    devices_json = request.form.get("devices_json","[]")
+    devices = json.loads(devices_json)
+    manual_commands = request.form.get("manual_commands", "").strip()
+
+    #General options
+    verbose_flag = "verbose" in request.form
+    verify_flag = "verify" in request.form
+    verbose_bool = True if verbose_flag else False
+    verify_bool = True if verify_flag else False
+
+    context = {
+        "device_file": device_file.filename if device_file else None,
+        "commands_file": commands_file.filename if commands_file else None,
+        "devices": devices,
+        "manual_commands": manual_commands,
+        "verbose": verbose_bool,
+        "verify": verify_bool,
+    }
+
+    return render_template("rollout.html", **context)
+
+
+def active_config():
+    pass
+
 
 
 if __name__ == "__main__":
